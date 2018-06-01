9c2d4eb93e8ab42f425470e4609c21afcf4d2d33
Sean Caron <scaron@umich.edu>
2018-06-01 15:11:41 -0400

    Update comments

--- mdfindunused.sh
+++ mdfindunused.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Locate all member drives of an MD array given the UUID of the array
+# Locate all drives that are not already part of an MD array
 # Sean Caron <scaron@umich.edu>
 
 # Run this script like:
-#  ./mdfinddrives.sh <uuid_of_array>
+#  ./mdfindunused.sh <uuid_of_array>
 
 for drive in /dev/sd[b-z] /dev/sd[a-z][a-z] ; do
     grep `basename $drive` /proc/mdstat &>/dev/null

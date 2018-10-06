4d4607b4798bbbd3e7e90c7eebbcf4aa093948ba
Sean Caron <scaron@umich.edu>
2018-10-05 22:23:56 -0400

    Correct algorithm for obtaining major and minor numbers

--- beacon.go
+++ beacon.go
@@ -12,6 +12,10 @@ import (
     "path/filepath"
 )
 
+var ( MINORBITS = uint(20)
+      MINORMASK = uint((uint(1) << MINORBITS) - 1)
+)
+
 func main() {
     if (len(os.Args) != 3) {
         fmt.Printf("Usage: %s disk [on|off]\n", os.Args[0])
@@ -22,9 +26,9 @@ func main() {
     stat := syscall.Stat_t{}
     
     _ = syscall.Stat("/dev/" + os.Args[1], &stat)
-   
-    major := uint64(stat.Rdev/256)
-    minor := uint64(stat.Rdev%256)
+
+    major := uint64((stat.Rdev >> 8) & 0xfff)
+    minor := uint64((stat.Rdev & 0xff) | ((stat.Rdev >> 12) & 0xfff00))
 
     // Construct the path to the fault LED control file
     pathToDrive := "/sys/dev/block/" + strconv.FormatUint(major,10) + ":" + strconv.FormatUint(minor,10) + "/device/block/" + os.Args[1] + "/device/enclosure*/fault"

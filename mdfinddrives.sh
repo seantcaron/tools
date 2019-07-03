1aaa5eebc95f947b300fa1d65f0b9cf5152ebe1c
root <root@topmed7.csgstat.sph.umich.edu>
2019-07-03 16:14:58 -0400

    Add drive count and automatically construct mdadm assemble command

--- mdfinddrives.sh
+++ mdfinddrives.sh
@@ -6,11 +6,23 @@
 # Run this script like:
 #  ./mdfinddrives.sh <uuid_of_array>
 
+i=0
+array=`grep $1 /etc/mdadm/mdadm.conf | cut -d " " -f 2 | cut -d "/" -f 4`
+cmd="mdadm --assemble --force /dev/md$array "
+
 for drive in /dev/sd[b-z] /dev/sd[a-z][a-z] ; do
-    mdadm --examine $drive | grep $1 &>/dev/null
+    mdadm --examine $drive | grep $1 &> /dev/null
 
     if [ $? == 0 ]; then
         echo $drive
+        i=$((i+1))
+        cmd+=$drive
+        cmd+=" "
     fi
 done
 
+echo "Found $i drives"
+
+echo "Reconstruct the array with the command:"
+
+echo $cmd

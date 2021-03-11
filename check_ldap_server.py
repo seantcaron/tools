98d2d40d8e562964f18140a839f002af53790d4b
Sean Caron <scaron@umich.edu>
2021-03-11 15:06:56 -0500

    Initial commit for check_ldap-server.py script

---
+++ check_ldap_server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+#
+# Check to see if OpenLDAP is running. If not, restart it and reset process limits.
+#
+# Install this script in cron as follows:
+# *       *       *       *       *       /root/check_ldap_server.py
+#
+
+# Requires packages: python-ldap
+import ldap, os, time
+
+l = ldap.initialize('ldap://' + 'csgadmin.sph.umich.edu' + ':389')
+try:
+    l.simple_bind()
+except:
+    os.system('/etc/init.d/slapd restart 2>&1 > /dev/null')
+    os.system('/usr/local/bin/change_limits $(pgrep slapd) 1000000 1000000 2>&1 > /dev/null')
+
+    log = open('/var/log/check_ldap_server.log', 'a+')
+    log_time = time.strftime("%A %b %d %H:%M:%S %Z", time.localtime())
+    log.write(log_time + ': Restarted OpenLDAP server\n')
+    log.close()
+
+    sys.exit(0)
+
+l.unbind()

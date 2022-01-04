058705897db65af3ac60c983d0f5c40e0874c4e6
Sean Caron <scaron@umich.edu>
2022-01-04 17:15:07 -0500

    Add a comment describing output format

--- checksmartdetailed.sh
+++ checksmartdetailed.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+#
+# Output will be of the format:
+# [device name] [SMART overall status] [raw read error rt] [reallocated sector ct] [reallocated event ct] [current pending] [offline uncorrectable] [udma crc error ct]
+#
+
 for drive in `lsscsi|grep -v PERC|grep -v DVD|tr -s ' '| cut -f 7 -d ' '` ; do
     { echo $drive | tr '\n' ' '; smartctl -H $drive | grep overall-health | cut -d ' ' -f 6 | tr '\n' ' '; smartctl -A $drive | grep '^  1\|^  5\|^196\|^197\|^198\|^199' | tr -s ' ' | awk '{$1=$1};1' | cut -d ' ' -f 10 | tr '\n' ' '; echo; } | cat >> output
 done

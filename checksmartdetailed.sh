53b3ea87dc44f64aed9128a5eaf1a460219f0710
Sean Caron <scaron@umich.edu>
2022-01-04 17:53:27 -0500

    Correct typo

--- checksmartdetailed.sh
+++ checksmartdetailed.sh
@@ -6,5 +6,5 @@
 #
 
 for drive in `lsscsi|grep -v PERC|grep -v DVD|tr -s ' '| cut -f 7 -d ' '` ; do
-    { echo $drive | tr '\n' ' '; mdadm --examine /dev/sdal | grep Name | awk '{$1=$1};1' | cut -d ' ' -f 3 | cut -d ':' -f 2 | sed -e 's/^/\/dev\/md/' | tr '\n' ' '; smartctl -H $drive | grep overall-health | cut -d ' ' -f 6 | tr '\n' ' '; smartctl -A $drive | grep '^  1\|^  5\|^196\|^197\|^198\|^199' | tr -s ' ' | awk '{$1=$1};1' | cut -d ' ' -f 10 | tr '\n' ' '; echo; } | cat >> output
+    { echo $drive | tr '\n' ' '; mdadm --examine $drive | grep Name | awk '{$1=$1};1' | cut -d ' ' -f 3 | cut -d ':' -f 2 | sed -e 's/^/\/dev\/md/' | tr '\n' ' '; smartctl -H $drive | grep overall-health | cut -d ' ' -f 6 | tr '\n' ' '; smartctl -A $drive | grep '^  1\|^  5\|^196\|^197\|^198\|^199' | tr -s ' ' | awk '{$1=$1};1' | cut -d ' ' -f 10 | tr '\n' ' '; echo; } | cat >> output
 done

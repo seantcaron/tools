a73b1659804936a13cd8ec3b3ebd4dadce1e627a
Sean Caron <scaron@umich.edu>
2022-01-04 18:15:41 -0500

    Add different cases for md member drives and not

--- checksmartdetailed.sh
+++ checksmartdetailed.sh
@@ -6,5 +6,19 @@
 #
 
 for drive in `lsscsi|grep -v PERC|grep -v DVD|tr -s ' '| cut -f 7 -d ' '` ; do
-    { echo $drive | tr '\n' ' '; mdadm --examine $drive | grep Name | awk '{$1=$1};1' | cut -d ' ' -f 3 | cut -d ':' -f 2 | sed -e 's/^/\/dev\/md/' | tr '\n' ' '; smartctl -H $drive | grep overall-health | cut -d ' ' -f 6 | tr '\n' ' '; smartctl -A $drive | grep '^  1\|^  5\|^196\|^197\|^198\|^199' | tr -s ' ' | awk '{$1=$1};1' | cut -d ' ' -f 10 | tr '\n' ' '; echo; } | cat >> output
+    # Member of md array
+    if mdadm --examine $drive > /dev/null 2>&1; then
+        { echo $drive | tr '\n' ' ';
+        mdadm --examine $drive | grep Name | awk '{$1=$1};1' | cut -d ' ' -f 3 | cut -d ':' -f 2 | sed -e 's/^/\/dev\/md/' | tr '\n' ' ';
+        smartctl -H $drive | grep overall-health | cut -d ' ' -f 6 | tr '\n' ' ';
+        smartctl -A $drive | grep '^  1\|^  5\|^196\|^197\|^198\|^199' | tr -s ' ' | awk '{$1=$1};1' | cut -d ' ' -f 10 | tr '\n' ' ';
+        echo; } | cat >> output
+    # Not member of md array
+    else
+        { echo $drive | tr '\n' ' ';
+        echo "None" | tr '\n' ' ';
+        smartctl -H $drive | grep overall-health | cut -d ' ' -f 6 | tr '\n' ' ';
+        smartctl -A $drive | grep '^  1\|^  5\|^196\|^197\|^198\|^199' | tr -s ' ' | awk '{$1=$1};1' | cut -d ' ' -f 10 | tr '\n' ' ';
+        echo; } | cat >> output
+    fi
 done

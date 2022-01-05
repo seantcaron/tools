3c0f9745b2bccb6ca58742c65c4a347047184289
Sean Caron <scaron@umich.edu>
2022-01-04 19:33:40 -0500

    Tweak output

--- checksmartdetailed.sh
+++ checksmartdetailed.sh
@@ -16,7 +16,7 @@ for drive in `lsscsi|grep -v PERC|grep -v DVD|tr -s ' '| cut -f 7 -d ' '` ; do
     # Not member of md array
     else
         { echo $drive | tr '\n' ' ';
-        echo "None" | tr '\n' ' ';
+        echo "NONE" | tr '\n' ' ';
         smartctl -H $drive | grep overall-health | cut -d ' ' -f 6 | tr '\n' ' ';
         smartctl -A $drive | grep '^  1\|^  5\|^196\|^197\|^198\|^199' | tr -s ' ' | awk '{$1=$1};1' | cut -d ' ' -f 10 | tr '\n' ' ';
         echo; } | cat >> output

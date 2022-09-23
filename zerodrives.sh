3c3fdeba392d58b611d4f6771bd690795c3288ad
Sean Caron <scaron@umich.edu>
2022-09-23 15:48:32 -0400

    Improve mechanism for identifying drives to loop over

--- zerodrives.sh
+++ zerodrives.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-for drive in /dev/sd[b-z] /dev/sd[a-z][a-z] ; do
+IFS=$'\n'
+
+for line in `/usr/bin/lsscsi | /bin/grep /dev/ | /bin/grep -Ev 'PERC|Dell|DELL|HP|LSI|DVD|Virtual|enclosu'`; do
+    # Number of fields can vary so be sure to always grab the last possible field (device name)
+    field_ct=`echo $line | /usr/bin/tr -s ' ' | /usr/bin/awk -F ' ' '{print NF; exit}'`
+    drive=`echo $line | /usr/bin/tr -s ' ' | /usr/bin/cut -f $field_ct -d ' '`
+    
     mdadm --zero-superblock $drive
     dd if=/dev/zero of=$drive bs=512 count=1024
 done

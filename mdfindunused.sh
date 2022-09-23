6c3f6d16a82c487037980b8ac095ac674b9fa201
Sean Caron <scaron@umich.edu>
2022-09-23 15:47:41 -0400

    Update mechanism for identifying drives to loop over and refine regexp used for the grep operation

--- mdfindunused.sh
+++ mdfindunused.sh
@@ -6,8 +6,14 @@
 # Run this script like:
 #  ./mdfindunused.sh <uuid_of_array>
 
-for drive in /dev/sd[b-z] /dev/sd[a-z][a-z] ; do
-    grep `basename $drive` /proc/mdstat &>/dev/null
+IFS=$'\n'
+
+for line in `/usr/bin/lsscsi | /bin/grep /dev/ | /bin/grep -Ev 'PERC|Dell|DELL|HP|LSI|DVD|Virtual|enclosu'`; do
+    # Number of fields can vary so be sure to always grab the last possible field (device name)
+    field_ct=`echo $line | /usr/bin/tr -s ' ' | /usr/bin/awk -F ' ' '{print NF; exit}'`
+    drive=`echo $line | /usr/bin/tr -s ' ' | /usr/bin/cut -f $field_ct -d ' '`
+    
+    grep `basename $drive`\\[ /proc/mdstat &>/dev/null
 
     if [ $? != 0 ]; then
         echo $drive

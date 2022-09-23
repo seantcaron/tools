0990caa5cbc5b466035d35891c726c9d35479a11
Sean Caron <scaron@umich.edu>
2022-09-23 15:38:21 -0400

    Improve mechanism for finding drives to loop over

--- beacon_off.sh
+++ beacon_off.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-for drive in `lsscsi|tr -s ' '| cut -f 7 -d ' '` ; do
+
+IFS=$'\n'
+
+for line in `/usr/bin/lsscsi | /bin/grep /dev/ | /bin/grep -Ev 'PERC|Dell|DELL|HP|LSI|DVD|Virtual|enclosu'`; do
+    # Number of fields can vary so be sure to always grab the last possible field (device name)
+    field_ct=`echo $line | /usr/bin/tr -s ' ' | /usr/bin/awk -F ' ' '{print NF; exit}'`
+    drive=`echo $line | /usr/bin/tr -s ' ' | /usr/bin/cut -f $field_ct -d ' '`
+    
+    # Arm fault LED for that drive
     ./beacon `basename $drive` off
 done
-

5ad5ec1f80a203a630bfbe99265238b46013d8b3
Sean Caron <scaron@umich.edu>
2022-10-26 21:54:02 -0400

    Initial commit

---
+++ checksumming/run_og_checksums.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=32G
+#SBATCH --time=28-00:00:00
+#SBATCH --chdir=/net/dumbo/home/scaron/disk1_checksumming_work
+#SBATCH --job-name=shasumog
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=scaron@umich.edu
+#SBATCH --array=1-100
+
+for file in `cat filelist_${SLURM_ARRAY_TASK_ID}` ; do
+  sha1sum $file >> shasumcheckog_${SLURM_ARRAY_TASK_ID}
+done

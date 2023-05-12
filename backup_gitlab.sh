0166e0708076653dbeb386c657ce1136510fcebc
Sean Caron <scaron@umich.edu>
2023-05-12 15:07:56 -0400

    Create backup_gitlab.sh
    Initial commit

---
+++ backup_gitlab.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#
+# Bundle together a Gitlab backup, a Gitlab /etc/gitlab backup and copying the files off to a
+#  remote destination.
+#
+# Run once weekly on Sunday at midnight by inserting the following into crontab for root:
+#  0 0 * * 0 /root/cron/backup_gitlab.sh
+#
+
+# Our hostname
+bk_host=`/bin/hostname`
+
+# Backup date
+bk_date=`/bin/date --iso-8601`
+
+# Backup destination directory
+bk_dest_root=/net/ddn/backup2/gitlab.backups
+bk_dest_dir=${bk_dest_root}/${bk_date}
+
+# Backup name to pass to gitlab-backup
+bk_name=${bk_host}_${bk_date}
+
+# Run the Gitlab backup
+/usr/bin/gitlab-backup create STRATEGY=copy BACKUP=$bk_name CRON=1
+gitlab_backup_file=/var/opt/gitlab/backups/${bk_name}_gitlab_backup.tar
+
+# Run the Gitlab /etc/gitlab backup
+
+gitlab_etc_backup_file=/tmp/${bk_name}_gitlab_etc_backup.tar
+/bin/tar -c -p -f $gitlab_etc_backup_file /etc/gitlab >/dev/null 2>&1
+
+# Copy data to the backup destination
+mkdir -p $bk_dest_dir
+cp $gitlab_backup_file $bk_dest_dir
+cp $gitlab_etc_backup_file $bk_dest_dir
+
+# Clean up
+/bin/rm -rf $gitlab_backup_file
+/bin/rm -rf $gitlab_etc_backup_file

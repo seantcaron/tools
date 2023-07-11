bebd7488e6008e1dad90d616d4c7fe54db073df4
Sean Caron <scaron@umich.edu>
2023-07-11 14:34:43 -0400

    Use Bioconductor instead, add comments

--- install.R
+++ install.R
@@ -1,21 +1,42 @@
+#!/usr/bin/env Rscript
+#
+# install.R: Install a list of R libraries from a file
 #
 # Prep input with:
 #   cd {R_library_repository}
 #   ls -l | tr -s ' ' | cut -d ' ' -f 9 > R.libraries
 #
+# Setup:
+#   If this is for a personal repository in a home directory, set R_LIBS_USER
+#   Otherwise leave R_LIBS_USER unset
+#
 # Run with:
-#   Rscript install.R
+#   Rscript install.R {manifest}
 #
 
 library(readr)
 
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args) != 1) {
+    stop("Usage: Rscript install.R {manifest}", call.=FALSE)
+}
+
+manifest <- args[1]
+
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install(version = "3.17")
+
 offset <- 0
 
 while (TRUE) {
-    line = read_lines("R.libraries", skip = offset, n_max = 1)
+    line = read_lines(manifest, skip = offset, n_max = 1)
     if (length(line) == 0) {
         break
     }
-    install.packages(line, dependencies=TRUE)
+
+    BiocManager::install(line,force=TRUE)
+
     offset <- offset + 1
 }
